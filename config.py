91138886a87be24019638dba260b83b36e3b48c2
Francesco Truono <f.truono5@gmail.com>
2020-12-29 15:37:21 +0100

    update link ml

--- config.py
+++ config.py
@@ -4,17 +4,18 @@
 
 import os
 
+
 class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 8000
-    APP_ID ="d85be113-3517-4cc4-864e-fbfde769ef95"
+    APP_ID = "d85be113-3517-4cc4-864e-fbfde769ef95"
     APP_PASSWORD = "q_GW.isZYCoGq628I6~.B1EnUa~ZtZT88b"
 
     SUBSCRIPTION_ID = '2853834e-9533-4f4f-80cb-9fba1589b89c'
     RESOURCE_GROUP = 'resourcegrouplinux'
     WORKSPACE_NAME = 'ML22'
-    FILE_NAME='pre'
+    FILE_NAME = 'pre'
 
-    JSON_FILE_NAME='metadata'
-    MODEL_LINK_PREDICTION='http://2e7611b0-9187-498c-b5c1-b2c2df00254f.westeurope.azurecontainer.io/score'
+    JSON_FILE_NAME = 'metadata'
+    MODEL_LINK_PREDICTION = 'http://051310e9-4932-434d-9104-f6c895e9e701.westeurope.azurecontainer.io/score'
