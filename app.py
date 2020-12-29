aea2c57ad6c0ea55cf1e7e894aa95c1429b292dd
Francesco Truono <f.truono5@gmail.com>
2020-12-29 15:06:18 +0100

    change startup and configuration

--- app.py
+++ app.py
@@ -96,11 +96,15 @@ async def messages(req: Request) -> Response:
     except Exception as exception:
         raise exception
 
+def init_func(argv):
+    APP = web.Application(middlewares=[aiohttp_error_middleware])
+    APP.router.add_post("/api/messages", messages)
+    return APP
 
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+
 
 if __name__ == "__main__":
+    APP=init_func(None)
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
--- config.py
+++ config.py
@@ -7,12 +7,12 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "d85be113-3517-4cc4-864e-fbfde769ef95")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "cc16b0d4-c880-4a11-a650-3bcfb067c6d5")
+    PORT = 8000
+    APP_ID ="d85be113-3517-4cc4-864e-fbfde769ef95"
+    APP_PASSWORD = "q_GW.isZYCoGq628I6~.B1EnUa~ZtZT88b"
 
     SUBSCRIPTION_ID = '2853834e-9533-4f4f-80cb-9fba1589b89c'
-    RESOURCE_GROUP = 'DefaultResourceGroup-WEU'
+    RESOURCE_GROUP = 'resourcegrouplinux'
     WORKSPACE_NAME = 'ML22'
     FILE_NAME='pre'
 
