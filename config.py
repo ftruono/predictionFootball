f0ff77628f17b1419dccc72b3aad8d4e99dcd9e0
Francesco Truono <f.truono5@gmail.com>
2020-12-30 09:08:35 +0100

    restore original config

--- config.py
+++ config.py
@@ -4,18 +4,17 @@
 
 import os
 
-
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 8000
-    APP_ID = "20812a0e-8642-4bf6-b067-af92a6e6c55c"
-    APP_PASSWORD = "mYVl3IPH2E~g~g~sY.TWo.p_Mb8eZw7LG1"
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
 
     SUBSCRIPTION_ID = '2853834e-9533-4f4f-80cb-9fba1589b89c'
-    RESOURCE_GROUP = 'resourcegrouplinux'
+    RESOURCE_GROUP = 'DefaultResourceGroup-WEU'
     WORKSPACE_NAME = 'ML22'
-    FILE_NAME = 'pre'
+    FILE_NAME='pre'
 
-    JSON_FILE_NAME = 'metadata'
-    MODEL_LINK_PREDICTION = 'http://051310e9-4932-434d-9104-f6c895e9e701.westeurope.azurecontainer.io/score'
+    JSON_FILE_NAME='metadata'
+    MODEL_LINK_PREDICTION='http://2e7611b0-9187-498c-b5c1-b2c2df00254f.westeurope.azurecontainer.io/score'
